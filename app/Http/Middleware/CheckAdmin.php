6668ae9617de52ff5fae93dae214782b15ca1bde
Mark Briones <wp.markbriones@gmail.com>
2019-11-07 18:25:11 +0800

    added simple roles/permission for user/admin

---
+++ app/Http/Middleware/CheckAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+
+class CheckAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $userRoles = Auth::user()->roles->pluck('name');
+
+        if (!$userRoles->contains('Admin')) { // no persmission, thus redirect to user page
+            return redirect('/user');
+        }
+        return $next($request);
+    }
+}
